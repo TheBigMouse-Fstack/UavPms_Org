59355af562af16ae1b86c0f0672a3e26cd26460b
an343 <an3439201@gmail.com>
2026-05-20 21:26:53 +0700

    Khởi tạo Database với EF Core Tools

--- UavPms.Infrastructure/DependencyInjection.cs
+++ UavPms.Infrastructure/DependencyInjection.cs
@@ -1,6 +1,20 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using UavPms.Infrastructure.Persistence;
+
 namespace UavPms.Infrastructure;
 
-public class DependencyInjection
+public static class DependencyInjection
 {
-    
+    public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        // Truyền Connection String vào cấu hình UseNpgsql
+        services.AddDbContext<ApplicationDbContext>(options =>
+        {
+            options.UseNpgsql(configuration.GetConnectionString("DefaultConnection"));
+        });
+        
+        return services;
+    }
 }
